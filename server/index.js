87fd1f2b55c7835ba3e47ba8c8816743c38e1b02
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2021-09-20 15:19:36 -0400

    got into node and package management

---
+++ server/index.js
@@ -0,0 +1,16 @@
+//This program is a server that listens on port 3000 and prints hello world 
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {        //This is the function that is called (served)
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
