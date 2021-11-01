6e844aecb1baec1c8da118b64aa6b96ad0b93928
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2021-11-01 14:11:42 -0400

    syntax

--- server/models/posts.js
+++ server/models/posts.js
@@ -9,6 +9,7 @@ const list = [
         user_handle: "@johnsmith",
         isPublic: true,
     },
+
     { 
         src: "https://bulma.io/images/placeholders/1280x960.png",
         alt: "Placeholder image",
@@ -17,6 +18,7 @@ const list = [
         user_handle: "@vp",
         isPublic: true,
     },
+
     { 
         src: "https://scontent-lga3-2.xx.fbcdn.net/v/t31.18172-8/p720x720/886090_10100137903372610_773365632_o.jpg?_nc_cat=101&ccb=1-5&_nc_sid=ba80b0&_nc_ohc=DWVdkKM2PwQAX_tAc5p&_nc_ht=scontent-lga3-2.xx&oh=d9d284fef84a57b1a824932d5fd2da20&oe=618A0203",
         alt: "Purim in SUB #100",
@@ -25,6 +27,7 @@ const list = [
         user_handle: "@JewPaltz",
         isPublic: true,
     },
+
     { 
         src: "https://scontent-lga3-2.xx.fbcdn.net/v/t1.6435-9/s600x600/244029201_10167312248050347_4050463819121596219_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=oaf-csovSFEAX-I2hxQ&_nc_ht=scontent-lga3-2.xx&oh=8466fcd68032477fab99306ba1a6e800&oe=61895C64",
         alt: "Mug with slogan",
@@ -33,6 +36,7 @@ const list = [
         user_handle: "@JewPaltz",
         isPublic: true,
     },
+
     { 
         src: "https://scontent-lga3-2.xx.fbcdn.net/v/t1.6435-9/p180x540/242759506_10102663165018030_5506732176336636339_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=730e14&_nc_ohc=LJFWaOTJXvUAX-skiU3&_nc_ht=scontent-lga3-2.xx&oh=e6a03799ccf969c3b3fe62a7b266b8b9&oe=618C38F8",
         alt: "The family",
@@ -59,7 +63,6 @@ module.exports.GetWall =  function GetWall(handle) {
 module.exports.GetFeed =  function GetFeed(handle) { return listWithOwner()
     .filter(post=> GetByHandle(handle).following.some(f=> f.handle == post.user_handle && f.isApproved) );     }
 
-
 module.exports.Get =  function Get(post_id) { return list[post_id]; }
 module.exports.Add =  function Add(post) {
     if(!post.user_handle){
@@ -68,12 +71,14 @@ module.exports.Add =  function Add(post) {
      list.push(post);
      return { ...post };
 }
+
 module.exports.Update =  function Update(post_id, post) {
     const oldObj = list[post_id];
     const newObj = { ...oldObj, ...post }
     list[post_id] = newObj ;
     return newObj;
 }
+
 module.exports.Delete =  function Delete(post_id) {
     const post = list[post_id];
     list.splice(post_id, 1);
