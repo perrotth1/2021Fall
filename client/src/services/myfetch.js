f4583e8cd355f6980deb04f12761b2a4b2569295
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2021-11-15 15:16:01 -0500

    help me

--- client/src/services/myfetch.js
+++ client/src/services/myfetch.js
@@ -1,10 +1,13 @@
 import session from "./session"
 
-const API_ROOT = 'a' + process.env.VUE_APP_API_ROOT ?? 'http://localhost:3100/'
+const API_ROOT = 'a' + process.env.VUE_APP_API_ROOT ?? 'http://localhost:3100/';
 
 export async function api(url){
     try {
         const x = await fetch(API_ROOT + url);
+        if(!x.ok) {
+            throw await x.json()
+        }
         return await x.json();
     } catch (err) {
         return session.Error(err);
