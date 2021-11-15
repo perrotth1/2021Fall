81e3ab4746728e6c179b24d78a6cc2b1930554e5
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2021-11-15 14:32:15 -0500

    added error handling

--- client/src/services/myfetch.js
+++ client/src/services/myfetch.js
@@ -1,6 +1,12 @@
+import session from "./session"
 
-const API_ROOT = 'http://localhost:3100/'
+const API_ROOT = 'a' + process.env.VUE_APP_API_ROOT ?? 'http://localhost:3100/'
 
-export function api(url){
-    return fetch(API_ROOT + url).then(x => x.json())
+export async function api(url){
+    try {
+        const x = await fetch(API_ROOT + url);
+        return await x.json();
+    } catch (err) {
+        return session.Error(err);
+    }
 }
