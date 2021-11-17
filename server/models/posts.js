ccab3acd71e01dad66f583a0c5efc743133a75e1
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2021-11-17 15:50:40 -0500

    fixed env error

--- server/models/posts.js
+++ server/models/posts.js
@@ -2,7 +2,7 @@ const Users = require("./users");
 const { ObjectId } = require("bson");
 const { client } = require("./mongo");
 
-const collection = client.db(process.env.MONGO_DB).collection('posts');
+const collection = client.db(process.env.MONGO_DBNAME).collection('posts');
 module.exports.collection = collection;
 
 const list = [
