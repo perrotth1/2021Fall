c2416dc656b372234ee73d3a7c9600c42ec5378f
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2021-11-17 15:11:01 -0500

    added async functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,20 +6,58 @@ const app = express.Router();       //a "sub pipeline"
 
 app
     .get("/", (req, res, next) => {
-        res.send(model.GetAll());
+        model   .GetAll()
+                .then( x => res.send(x) )
+                .catch(next)
     })
+
+    .get("/wall/:handle", (req, res, next) => {
+        model   .GetWall(req.params.handle)
+                .then( x => res.send(x) )
+                .catch(next)
+    })
+
+    .get("/feed/:handle", (req, res, next) => {
+        model   .GetFeed(req.params.handle)
+                .then( x => res.send(x) )
+                .catch(next)
+    })
+
+
     .get("/search", (req, res, next) => {
-        res.send(model.Search(req.query.q));
+        model   .Search(req.query.q)
+                .then( x => res.send(x) )
+                .catch(next)
     })
+
     .get("/:id", (req, res, next) => {
-        console.log(req.headers);
-
-        res.send(model.Get(req.params.id));
+        model   .Get(req.params.id)
+                .then( x => res.send(x) )
+                .catch(next)
     })
-    .post("/", (req, res, next) => {
-        const newPost = model.Add(req.body);
 
-        res.status(201).send(newPost);
+    .post("/", (req, res, next) => {
+        model   .Add(req.body)
+                .then( x => res.status(201).send(x) )
+                .catch(next)
+    })
+
+    .patch("/:id", (req, res, next) => {
+        model   .Update(req.params.id, req.body)
+                .then( x => res.send(x) )
+                .catch(next)
+    })
+
+    .delete("/:id", (req, res, next) => {
+        model   .Delete(req.params.id)
+                .then( x => res.send({ deleted: x }) )
+                .catch(next)
+    })
+
+    .post("/seed", (req, res, next) => {
+        model   .Seed()
+                .then( x => res.status(201).send(x) )
+                .catch(next)
     })
 
 
